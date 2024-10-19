08e03d31377d34d8ba3b2916831843a2ece9b101
yomek33 <i3landme@gmail.com>
2024-10-19 21:37:19 +0900

    Refactor userHandler to remove unused code

--- internal/handler/users.go
+++ internal/handler/users.go
@@ -1,18 +1,15 @@
 package handler
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
-	"github.com/yomek33/talki/internal/logger"
-	"github.com/yomek33/talki/internal/models"
 	"github.com/yomek33/talki/internal/services"
 )
 
+// now, use only GetGoogleLoginSignin
 type UserHandler interface {
-	CreateUser(c echo.Context) error
-	UpdateUser(c echo.Context) error
-	DeleteUser(c echo.Context) error
+	// CreateUser(c echo.Context) error
+	// UpdateUser(c echo.Context) error
+	// DeleteUser(c echo.Context) error
 	GetGoogleLoginSignin(c echo.Context) error
 }
 
@@ -23,64 +20,64 @@ type userHandler struct {
 }
 
 // Handlers
-func (h *userHandler) CreateUser(c echo.Context) error {
-	var user models.User
-	if err := c.Bind(&user); err != nil {
-		logger.Errorf("Error binding user data: %v", err)
-		return respondWithError(c, http.StatusBadRequest, ErrInvalidUserData)
-	}
-	if err := validateUser(&user); err != nil {
-		logger.Errorf("Error validating user data: %v", err)
-		return respondWithError(c, http.StatusBadRequest, err.Error())
-	}
+// func (h *userHandler) CreateUser(c echo.Context) error {
+// 	var user models.User
+// 	if err := c.Bind(&user); err != nil {
+// 		logger.Errorf("Error binding user data: %v", err)
+// 		return respondWithError(c, http.StatusBadRequest, ErrInvalidUserData)
+// 	}
+// 	if err := validateUser(&user); err != nil {
+// 		logger.Errorf("Error validating user data: %v", err)
+// 		return respondWithError(c, http.StatusBadRequest, err.Error())
+// 	}
 
-	if err := h.UserService.CreateUser(&user); err != nil {
-		logger.Errorf("Error creating user: %v", err)
-		return respondWithError(c, http.StatusInternalServerError, ErrCouldNotCreateUser)
-	}
-	logger.Infof("User created successfully")
-	return c.JSON(http.StatusCreated, user)
-}
+// 	if err := h.UserService.CreateUser(&user); err != nil {
+// 		logger.Errorf("Error creating user: %v", err)
+// 		return respondWithError(c, http.StatusInternalServerError, ErrCouldNotCreateUser)
+// 	}
+// 	logger.Infof("User created successfully")
+// 	return c.JSON(http.StatusCreated, user)
+// }
 
-func (h *userHandler) UpdateUser(c echo.Context) error {
-	UserUID, err := getUserUIDFromContext(c)
-	if err != nil {
-		logger.Errorf("Error getting user UID from context: %v", err)
-		return c.JSON(http.StatusUnauthorized, echo.Map{"error": ErrInvalidUserToken})
-	}
+// func (h *userHandler) UpdateUser(c echo.Context) error {
+// 	UserUID, err := getUserUIDFromContext(c)
+// 	if err != nil {
+// 		logger.Errorf("Error getting user UID from context: %v", err)
+// 		return c.JSON(http.StatusUnauthorized, echo.Map{"error": ErrInvalidUserToken})
+// 	}
 
-	var user models.User
-	if err := c.Bind(&user); err != nil {
-		logger.Errorf("Error binding user data: %v", err)
-		return respondWithError(c, http.StatusBadRequest, ErrInvalidUserData)
-	}
-	if err := validateUser(&user); err != nil {
-		logger.Errorf("Error validating user data: %v", err)
-		return respondWithError(c, http.StatusBadRequest, err.Error())
-	}
-	if user.UserUID != UserUID {
-		logger.Errorf("Mismatch between user UID in context and request")
-		return respondWithError(c, http.StatusBadRequest, ErrInvalidUserUID)
-	}
-	if err := h.UserService.UpdateUser(&user); err != nil {
-		logger.Errorf("Error updating user: %v", err)
-		return respondWithError(c, http.StatusInternalServerError, ErrCouldNotUpdateUser)
-	}
-	logger.Infof("User updated successfully")
-	return c.JSON(http.StatusOK, user)
-}
+// 	var user models.User
+// 	if err := c.Bind(&user); err != nil {
+// 		logger.Errorf("Error binding user data: %v", err)
+// 		return respondWithError(c, http.StatusBadRequest, ErrInvalidUserData)
+// 	}
+// 	if err := validateUser(&user); err != nil {
+// 		logger.Errorf("Error validating user data: %v", err)
+// 		return respondWithError(c, http.StatusBadRequest, err.Error())
+// 	}
+// 	if user.UserUID != UserUID {
+// 		logger.Errorf("Mismatch between user UID in context and request")
+// 		return respondWithError(c, http.StatusBadRequest, ErrInvalidUserUID)
+// 	}
+// 	if err := h.UserService.UpdateUser(&user); err != nil {
+// 		logger.Errorf("Error updating user: %v", err)
+// 		return respondWithError(c, http.StatusInternalServerError, ErrCouldNotUpdateUser)
+// 	}
+// 	logger.Infof("User updated successfully")
+// 	return c.JSON(http.StatusOK, user)
+// }
 
-func (h *userHandler) DeleteUser(c echo.Context) error {
-	UserUID, err := getUserUIDFromContext(c)
-	if err != nil {
-		logger.Errorf("Error getting user UID from context: %v", err)
-		return respondWithError(c, http.StatusUnauthorized, ErrInvalidUserToken)
-	}
+// func (h *userHandler) DeleteUser(c echo.Context) error {
+// 	UserUID, err := getUserUIDFromContext(c)
+// 	if err != nil {
+// 		logger.Errorf("Error getting user UID from context: %v", err)
+// 		return respondWithError(c, http.StatusUnauthorized, ErrInvalidUserToken)
+// 	}
 
-	if err = h.UserService.DeleteUser(UserUID); err != nil {
-		logger.Errorf("Error deleting user: %v", err)
-		return respondWithError(c, http.StatusInternalServerError, ErrCouldNotDeleteUser)
-	}
-	logger.Infof("User deleted successfully")
-	return c.NoContent(http.StatusNoContent)
-}
+// 	if err = h.UserService.DeleteUser(UserUID); err != nil {
+// 		logger.Errorf("Error deleting user: %v", err)
+// 		return respondWithError(c, http.StatusInternalServerError, ErrCouldNotDeleteUser)
+// 	}
+// 	logger.Infof("User deleted successfully")
+// 	return c.NoContent(http.StatusNoContent)
+// }
